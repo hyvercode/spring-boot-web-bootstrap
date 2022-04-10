ba0a307084de494c5e839a165ea1acb93399f5db
mohirwanh <mohirwan@gmail.com>
2022-04-10 22:01:42 +0700

    2022-04-10

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String index(){
+		return "index.html";
+	}
+
+	@GetMapping("/bukutamu")
+	public String book(){
+		return "bukutamu.html";
+	}
+
+	@GetMapping("/daftar-bukutamu")
+	public String daftar(){
+		return "daftar-bukutamu.html";
+	}
+}
---
+++ src/main/java/com/example/demo/model/BukuTamu.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+public class BukuTamu {
+
+    private String name;
+    private String phoneNumber;
+    private String alamat;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
